9793ab9c5212ac396ff4d4490c0e56883806e834
Israel Akintunde <israelakintunde005@gmail.com>
2024-02-10 03:49:54 +0100

    test

--- app/main.js
+++ app/main.js
@@ -1,7 +1,13 @@
 const net = require("net");
 
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on('data', data => {
+    sendPongResponse(connection);
+  })
 });
 
 server.listen(6379, "127.0.0.1");
+
+const sendPongResponse = socket => {
+  socket.write('+PONG\r\n');
+}
